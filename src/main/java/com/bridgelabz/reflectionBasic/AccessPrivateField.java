b4b7d5056f73e27a2f177dfc2da9df4caf39a432
Yagyata <yagyatasharmaa@gmail.com>
2025-04-05 14:08:11 +0530

    [Yagyata] ADD: Access Private Field

---
+++ src/main/java/com/bridgelabz/reflectionBasic/AccessPrivateField.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.reflectionBasic;
+
+import java.lang.reflect.Field;
+
+class Person {
+    private int age = 20;
+}
+
+public class AccessPrivateField {
+    public static void main(String[] args) throws Exception {
+        Person person = new Person();
+        Class<?> cls = person.getClass();
+
+        Field field = cls.getDeclaredField("age");
+        field.setAccessible(true);
+
+        System.out.println("Before Modifying: Age = " + field.get(person));
+
+        //Modifying the value
+        field.set(person, 21);
+
+        //Retrieving the value
+        System.out.println("After Modifying: Age = " + field.get(person));
+    }
+}
