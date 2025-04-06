9f2a5adda1afca93e0c9c337f26785bc9c688f70
Yagyata <yagyatasharmaa@gmail.com>
2025-04-06 19:38:35 +0530

    [Yagyata] ADD: Access and Modify Static Fields

---
+++ src/main/java/com/bridgelabz/reflectionIntermediate/ModifyStaticFields.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.reflectionIntermediate;
+
+import java.lang.reflect.Field;
+
+class Configuration{
+    private static String API_KEY = "apiKey";
+}
+
+public class ModifyStaticFields {
+    public static void main(String[] args) throws Exception {
+        Configuration config = new Configuration();
+        Class<?> c = config.getClass();
+
+        Field f = c.getDeclaredField("API_KEY");
+        f.setAccessible(true);
+        System.out.println("Before Modifying, Value: " + f.get(Configuration.class));
+
+        f.set(Configuration.class, "apiKeyChanged");
+        System.out.println("After Modifying, Value: : " + f.get(Configuration.class));
+    }
+}
